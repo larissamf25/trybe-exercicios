9d01d43164401b3ae4284a6fb3e61d7b82e4b657
Larissa <larissamf25@gmail.com>
2022-04-27 15:50:07 -0300

    exercicios de fixação

---
+++ módulo-I-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/library.js
@@ -0,0 +1,8 @@
+// let moment = require("moment");
+// let brasilTetra = moment('17071994','DDMMYYYY');
+// let fromNow = brasilTetra.fromNow();
+// console.log("Deu certo "+ fromNow);
+
+// cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+//   alert('Voilà!');
+// });
