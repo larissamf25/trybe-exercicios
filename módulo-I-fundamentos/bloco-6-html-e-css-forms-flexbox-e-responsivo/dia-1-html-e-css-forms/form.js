1cbd551653941180a3476d54110e2c23cafab3c0
Larissa <larissamf25@gmail.com>
2022-04-27 19:27:24 -0300

    adicionando bootstrap ao formulário

--- módulo-I-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/form.js
+++ módulo-I-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/form.js
@@ -5,18 +5,19 @@ const nome = document.getElementById("nome-completo");
 const email = document.getElementById('input-email');
 
 submit.addEventListener("click", function(e) {
-  if(share.checked == false) {
-    e.preventDefault();
-    alert("Sorry, you can't submit your infos! Please check the box.");
-  } else if (nome.length < 10 || nome.length > 40 || email.length < 10 || email.length > 50) {
-    e.preventDefault();
-    alert("Dados inválidos");
-  } else {
-    alert("Dados enviados com sucesso! Obrigada por participar do concurso TrybeTrip.");
-  }
+  e.preventDefault();
+  // if(share.checked == false) {
+  //   e.preventDefault();
+  //   alert("Sorry, you can't submit your infos! Please check the box.");
+  // } else if (nome.length < 10 || nome.length > 40 || email.length < 10 || email.length > 50) {
+  //   e.preventDefault();
+  //   alert("Dados inválidos");
+  // } else {
+  //   alert("Dados enviados com sucesso! Obrigada por participar do concurso TrybeTrip.");
+  // }
 });
 
 clear.addEventListener("click", function(e) {
   e.preventDefault();
-  document.getElementById("form").reset();
+  //  document.getElementById("form").reset();
 })
