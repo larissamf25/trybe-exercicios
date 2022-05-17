055fdc475132d962bc11010d00f537fa4435195a
Larissa <larissamf25@gmail.com>
2022-05-17 15:10:01 -0300

    exercícios completos

--- módulo-I-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ módulo-I-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -62,21 +62,39 @@ const books = [
 ];
 
 // EXERCÍCIO 1
-
+const formatedBookNames = () => books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`)
+//  console.log(formatedBookNames());
 
 // EXERCÍCIO 2
-
+const nameAndAge = () => books.map((book) => ({age: book.releaseYear - book.author.birthYear, author: book.author.name})).sort((a,b) => a.age - b.age);
+//  console.log(nameAndAge());
 
 // EXERCÍCIO 3
-
+const fantasyOrScienceFiction = () => books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+//  console.log(fantasyOrScienceFiction());
 
 // EXERCÍCIO 4
-
+const oldBooksOrdered = () => books.filter((book) => 2022 - book.releaseYear > 60).sort((a,b) => a.releaseYear - b.releaseYear);
+//  console.log(oldBooksOrdered());
 
 // EXERCÍCIO 5
-
+const fantasyOrScienceFictionAuthors = () => books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica').map((book) => book.author.name).sort();
+//  console.log(fantasyOrScienceFictionAuthors());
 
 // EXERCÍCIO 6
-
+const oldBooks = () => books.filter((book) => 2022 - book.releaseYear > 60).map((book) => book.name);
+//  console.log(oldBooks());
 
 // EXERCÍCIO 7
+const authorWith3DotsOnName = () => books.find((book) => {
+  let count = 0;
+  for (let index = 0; index < book.author.name.length; index += 1) {
+    if (book.author.name[index] === '.') {
+      count += 1;
+    }
+  }
+  if (count === 3) {
+    return true;
+  }
+}).name;
+console.log(authorWith3DotsOnName());
