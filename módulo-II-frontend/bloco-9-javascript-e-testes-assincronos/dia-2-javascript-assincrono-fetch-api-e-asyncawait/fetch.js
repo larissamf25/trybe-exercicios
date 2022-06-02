e9f72a32da6d2fe08845f3fea7fa3374d31f44d6
Larissa <larissamf25@gmail.com>
2022-06-02 13:54:17 -0300

    finalizando o dia

--- módulo-II-frontend/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
+++ módulo-II-frontend/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
@@ -1,13 +1,24 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-const fetchJoke = async () => {
-  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  const result = await fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data.value))
-    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
-  console.log(result);
+//   const result = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+  //  console.log(result);
+// }
+
+// fetchJoke();
+
+function lastDigit(as){
+  if (as.length === 0) {
+    return 1;
+  }
+  let number = as.reverse().reduce((pot, n) => pot = Math.pow(pot, n));
+  console.log(number);
+  let n = number.toString();
+  return n.charAt(n.length - 1);
 }
-
-fetchJoke();
+console.log(lastDigit([3, 4, 5]));
