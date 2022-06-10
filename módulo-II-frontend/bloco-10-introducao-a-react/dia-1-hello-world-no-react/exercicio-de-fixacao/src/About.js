4af3322cc1878983275e2882493a7a24d3f4c2f2
Larissa <larissamf25@gmail.com>
2022-06-10 19:36:29 -0300

    exercício de fixação

---
+++ módulo-II-frontend/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-de-fixacao/src/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Larissa Menezes</h1>
+        <p>é uma desenvolvedora back-end</p>
+      </div>
+    );
+  }
+}
+  
+export default About;
