4f63189aef7f0deab2228aa87d7498ce5c2b7dc5
Larissa <larissamf25@gmail.com>
2022-07-13 12:09:45 -0300

    exercício de fixação

---
+++ módulo-II-frontend/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -0,0 +1,56 @@
+// App.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+describe('Tela de inserção de email', () => {
+  test('Verificando se existe o campo Email.', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
+  
+  test('Verificando se existe um botão de enviar', () => {
+    render(<App />);
+    const btnSend = screen.getByTestId('id-send');
+    expect(btnSend).toBeInTheDocument();
+    expect(btnSend).toHaveProperty('type', 'button');
+    expect(btnSend).toHaveValue('Enviar');
+  });
+  
+  test('Verificando se existe dois botões', () => {
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
+});
+
+// Adicione esse teste.
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
+});
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react tests/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
