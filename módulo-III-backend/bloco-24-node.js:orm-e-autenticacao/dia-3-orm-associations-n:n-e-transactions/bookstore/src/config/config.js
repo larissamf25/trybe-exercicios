bca745ec8dc29673fdb0b202ed25579aede99a19
Larissa <larissamf25@gmail.com>
2022-10-21 11:58:51 -0300

    exercício de fixação

---
+++ módulo-III-backend/bloco-24-node.js:orm-e-autenticacao/dia-3-orm-associations-n:n-e-transactions/bookstore/src/config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const config = {
+  username: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  host: process.env.SQL_HOST,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: {
+    ...config,
+    database: 'books_database_development',
+  },
+  test: {
+    ...config,
+    database: 'books_database_test',
+    logging: false,
+  },
+  production: {
+    ...config,
+    database: 'books_database_production',
+  },
+};
