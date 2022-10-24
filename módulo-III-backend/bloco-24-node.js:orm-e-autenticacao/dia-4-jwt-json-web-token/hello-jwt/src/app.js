19b01b94b792d1d27dcce6d2edab49be21b49882
Larissa <larissamf25@gmail.com>
2022-10-24 15:26:03 -0300

    exercício de prática

---
+++ módulo-III-backend/bloco-24-node.js:orm-e-autenticacao/dia-4-jwt-json-web-token/hello-jwt/src/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers');
+const middlewares = require('./middlewares');
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/ping', controllers.ping);
+app.get('/users/me', middlewares.auth, controllers.me);
+app.get('/top-secret', middlewares.auth, middlewares.admin, controllers.topSecret);
+app.post('/login', controllers.login);
+app.use(middlewares.error);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
