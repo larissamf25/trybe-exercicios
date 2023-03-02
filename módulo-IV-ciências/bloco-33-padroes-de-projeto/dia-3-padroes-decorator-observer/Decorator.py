2fa28852a16f992f58fcab3c0a71896e96a07783
Larissa <larissamf25@gmail.com>
2023-03-02 11:26:59 -0300

    exercícios de fixação

---
+++ módulo-IV-ciências/bloco-33-padroes-de-projeto/dia-3-padroes-decorator-observer/Decorator.py
@@ -0,0 +1,35 @@
+class Calculadora:
+    def soma(self, x, y):
+        return x + y
+
+
+class CalculadoraDecorada:
+    def __init__(self, calculadora):
+        self.calculadora = calculadora
+
+    def converterNumero(self, numero):
+        if not isinstance(numero, str):
+            return numero
+
+        # Neste cenário, em vez de fazermos IF e else... podemos usar o dicionário
+        # conseguimos acessar obter o valor a partir da chave
+        return {
+            "um": 1, "one": 1, "dois": 2, "two": 2, "três": 3, "three": 3,
+            "quatro": 4, "four": 4, "cinco": 5, "five": 5, "seis": 6, "six": 6,
+            "sete": 7, "seven": 7, "oito": 8, "eight": 8, "nove": 9, "nine": 9,
+            "dez": 10, "ten": 10
+        }.get(numero)
+
+    def soma(self, x, y):
+        return self.calculadora.soma(
+            self.converterNumero(x), self.converterNumero(y)
+        )
+
+
+if __name__ == "__main__":
+    calculadora = Calculadora()
+    print("10 + 20 =")
+    calculadora.soma(10, 20)
+
+    calculadoraDecorada = CalculadoraDecorada(calculadora)
+    print("'oito' + 'dois' =", calculadoraDecorada.soma("oito", "dois"))
